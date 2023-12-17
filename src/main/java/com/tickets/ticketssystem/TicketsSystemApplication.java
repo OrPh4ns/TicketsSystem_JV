72dd278e4ce1061a3326917292a9d5f6fffcfb76
AbdulxOthman <gazixq@gmail.com>
2023-12-17 22:57:35 +0100

    init update

---
+++ src/main/java/com/tickets/ticketssystem/TicketsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tickets.ticketssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tickets/ticketssystem/TicketsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickets.ticketssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
